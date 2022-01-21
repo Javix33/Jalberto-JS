6edc4f7a4082a4c1332c7c0b4b77b89fb3535d41
Javier Alberto <javix33@hotmail.com>
2022-01-20 18:13:03 -0600

    modificado js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let nombre = prompt("nombre completo");
+/*let nombre = prompt("nombre completo");
 let nacimiento = prompt("año de nacimiento cuatro cifras");
 //const ANO = new Date().getFullYear();
 //let edad = (ANO - nacimiento);
@@ -9,13 +9,24 @@ function x(nombre, nacimiento) {
   let edad = (ANO - nacimiento);
   let salida = `hola ${nombre} tienes ${edad}`;
   if (edad < 10) {
-    alert(`${salida} que bueno que puedas dedicar tiempo a jugar`);
+    console.log(`${salida} que bueno que puedas dedicar tiempo a jugar`);
   } else if (edad <= 18) {
-    alert(`${salida}  tienes toda una vida por delante`)
+    console.log(`${salida}  tienes toda una vida por delante`)
   } else if (edad > 18 && edad <= 35) {
-    alert(`${salida} que grande que estas`);
+    console.log(`${salida} que grande que estas`);
   } else if (edad > 35 && edad <= 65) {
-    alert(`${salida} estas muy grande para andar con jueguitos`);
-  } else { alert(`camara prro`) }
+    console.log(`${salida} estas muy grande para andar con jueguitos`);
+  } else { console.log(`camara prro`) }
 }
-x(nombre, nacimiento);
+x(nombre, nacimiento);*/
+
+/*for (i = 0; i < 100; i++) {
+  let salida = `se vendio la entrada ${i}`;
+  if (i <= 50) {
+    console.log(`${salida} quedan mas de la mitad de entradas`);
+  } else if (i > 50 && i < 75) {
+    console.log(`${salida} quedan pocas entradas`)
+  } else if (i >= 75 && i < 99) {
+    console.log(`${salida} las entradas estan apunto de agotarse`)
+  } else { console.log(`${salida} esta es la ultima entrada`) }
+}*/
