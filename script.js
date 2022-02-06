b23b9a4d8ca1ab29cd31933ab5485cad35bc22ec
Javier Alberto <javix33@hotmail.com>
2022-02-05 19:11:04 -0600

    estructura de proyecto, simulador

--- script.js
+++ script.js
@@ -38,4 +38,93 @@ switch (today) {
     console.log(`${welcome} no parece un mal dia para un poco de mezcal.`);
 };
 
-let
+
+let userBebida = prompt("¿Qué bebida de elixir utilizaras? (espadin o algun sabor 'Naranja, Maracuya o lichi'");
+userBebida = userBebida.toLowerCase();
+console.log(userBebida)
+if (userBebida == "naranja") {
+  console.log(userBebida)
+  console.log(`Solo añade hielo, algunos trozos de fruta y disfruta de tu Mezcal Elixir sabor Naranja`);
+} else if (userBebida == "maracuya") {
+  console.log(`Solo añade hielo, algunos trozos de fruta y disfruta de tu Mezcal Elixir sabor Maracuya`);
+} else if (userBebida == "lichi") {
+  console.log(`Solo añade hielo, algunos trozos de fruta y disfruta de tu Mezcal Elixir sabor Lichi`);
+} else {
+  console.log(`procedamos a preparar algo con tu Mezcal Espadin de Elixir`);
+};
+
+let volumen = prompt("¿Que cantidad quieres preparar?(ingresala en mililitros y solo números)");
+volumen = parseInt(volumen);
+volumen = volumen / 1000;
+
+class proporcion {
+  constructor(x) {
+    this.agua = x.agua * volumen;
+    this.mezcal = x.mezcal * volumen;
+    this.jamaica = x.jamaica * volumen;
+    this.tamarindo = x.tamarindo * volumen;
+    this.jugo_limon = x.jugo_limon * volumen;
+    this.miel = x.miel * volumen;
+    this.pepino = x.pepino * volumen;
+  };
+
+  recetaNueva() {
+    console.log(`para preparar ${volumen}l. de esta bebida necesitaras los siguientes ingredientes: Agua: ${this.agua}ml., Mezcal: ${this.mezcal}ml., Jamaica: ${this.jamaica}gr., Tamarindo: ${this.tamarindo}gr., Jugo de limon: ${this.jugo_limon}ml., Miel: ${this.miel} cucharadas.`);
+  };
+};
+
+let a = {
+  agua: 800,
+  mezcal: 64,
+  jugo_limon: 80,
+  pepino: 2,
+  miel: 5
+};
+//mezcalita de jamaica
+let b = {
+  agua: 800,
+  mezcal: 64,
+  jamaica: 80,
+  miel: 5
+};
+//mezcalita de tamarindo
+let c = {
+  agua: 800,
+  mezcal: 64,
+  Tamarindo: 80,
+  miel: 5
+};
+
+let receta = prompt("selecciona la receta que quieres preparar ingresando unicamente la letra de la que elijas, ejemplo, para preparar una mezcalita de 'jamaica' ingresa solo la letra 'b'");
+let preparar;
+
+switch (receta) {
+  case "a":
+    preparar = new proporcion(a);
+    preparar.recetaNueva();
+    break;
+  case "b":
+    preparar = new proporcion(b);
+    preparar.recetaNueva();
+    break;
+  case "c":
+    preparar = new proporcion(c);
+    preparar.recetaNueva();
+    break;
+  default:
+    console.log("ingresaste una letra que no coincide con ninguna receta");
+};
+volumen = volumen * 1000;
+
+let caballitosPorServir = prompt(`tienes ${volumen} ml de esta receta ¿cuantos caballitos serviras?(solo numero)`);
+const MILITROS_POR_CABALLITO = 30;
+let mililitrosPorRonda = caballitosPorServir * MILITROS_POR_CABALLITO;
+for (let i = volumen, r = 1; i > mililitrosPorRonda; i -= mililitrosPorRonda, r++) {
+  let rondas = Math.trunc((i / mililitrosPorRonda) - 1);
+  let salida = `Ronda ${r} serviste ${caballitosPorServir} caballitos de mezcal te quedan ${rondas} rondas`;
+  if (i >= 500) {
+    console.log(`${salida} eso es mas de la mitad, la noche es joven`);
+  } else if (i < 500 && i >= 300) {
+    console.log(`${salida} tu mezcal se esta agotando`);
+  } else { console.log(`${salida} es momento de conseguir mas elixir`); }
+};
